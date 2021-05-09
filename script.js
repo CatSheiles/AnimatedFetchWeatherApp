7672e475659aecb2af7ad4e8654501d9b08c8a4d
CatSheiles <cathysheiles@gmail.com>
2021-05-09 19:12:44 +1000

    initial

---
+++ script.js
@@ -0,0 +1,44 @@
+let weatherContainer = document.querySelector('.container');
+let searchBox = document.getElementById('searchBox');
+let searchButton = document.querySelector('button');
+
+let city = '';
+
+//let data = fetch("http://api.openweathermap.org/data/2.5/weather?q=Brisbane&units=metric&appid=6adf14186495bc9a9014337d3a070921")
+//    .then(response => response.json())
+//    .then(data => console.log(data));
+// cleaned it up instead of using default fetch, used async await
+
+async function GetWeather()
+{
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6adf14186495bc9a9014337d3a070921`);
+    let data = await response.json();
+    console.log(data);
+
+    let template = `
+        <h1 class="temp"> ${data.main.temp} &#8451;<h1>
+        <h2 class="city"> ${data.name} <h2>
+        <h2 class="desc"> ${data.weather[0].description} <h2>
+
+        <div class="extras">
+            <p>Feels like: ${data.main.feels_like} <p>
+            <p>Humidity: ${data.main.humidity} <p>
+            <p>Pressure: ${data.main.pressure} <p>
+            <p>Temp Min: ${data.main.temp_min} <p>
+            <p>Temp Max: ${data.main.temp_max} <p>
+            <p>Wind Speed: ${data.wind.speed} <p>
+        </div>
+    `;
+
+    weatherContainer.innerHTML += template
+}
+//GetWeather();
+
+searchButton.addEventListener('click', () => {
+    city = searchBox.value;
+    GetWeather();
+});
+
+
+
+//6adf14186495bc9a9014337d3a070921
